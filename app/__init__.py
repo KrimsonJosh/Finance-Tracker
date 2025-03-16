36b39ed570222aa2f66a8a55ad9a7adc46736dbc
KrimsonJosh <jumoinzad@gmail.com>
2025-03-16 17:05:43 -0500

    refactor: refactored into modules (blueprints)

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        from .routes.auth_routes import auth_bp
+        app.register_blueprint(auth_bp)
+
+        db.create_all()
+
+    return app
---
+++ app/routes/auth_routes.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from ..models import db, User
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/')
+def home():
+    return "Hello world"
+
+@auth_bp.route('/signup', methods=['GET', 'POST']) 
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('auth.home'))
+    return render_template('signup.html')
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            return redirect(url_for('auth.home'))
+        else:
+            return "Invalid Username or password", 401
+    return render_template('login.html')
