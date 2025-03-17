4c6ac7ae2b1f571852cb8f343bbf2a698c5ec2cb
KrimsonJosh <jumoinzad@gmail.com>
2025-03-17 12:59:05 -0500

    feat: add editexpense method

--- app/routes/dashboard_routes.py
+++ app/routes/dashboard_routes.py
@@ -8,7 +8,6 @@ def addExpense():
     amount = request.form.get('amount')
     category = request.form.get('category')
 
-
     if not amount or not category:
         return jsonify({'success': False, 'message': "Amount and category required"}), 400
     
@@ -29,9 +28,32 @@ def addExpense():
         db.session.rollback()
         return jsonify({'success': False, 'message': 'database error'}), 500
 
-@dashboard_bp.route('/api/expenses/<id>', methods = ['PUT'])
+@dashboard_bp.route('/api/expenses/<int:id>', methods = ['PUT'])
 def editExpense():
-    pass 
+    amount = request.form.get('amount')
+    category = request.form.get('category')
+
+    if not amount or not category:
+        return jsonify({'success': False, 'message': 'Amount and category required'}), 400
+    
+    try: 
+        amount = float(amount)
+    except ValueError:
+        return jsonify({'success': False, 'message': 'Amount must be a number'}), 400
+    try:
+        user_id = 1 # TODO: replace with session id latter
+        expense = Expense.query.filter_by(id=id, user_id = user_id).first()
+        if not expense:
+            return jsonify({'success': False, 'message': 'Expense not found'}), 404 
+        
+        expense.amount = amount 
+        expense.category = category
+
+        db.session.commit()
+        return jsonify({'success': True, 'message': 'expense updated successfully'}), 200 
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'success': False, 'message': 'database error'}), 500
 
 @dashboard_bp.route('/api/expenses/<id>', methods = ['DELETE'])
 def deleteExpense():
