a30f47ad0fbb0861bd8bf37c4b61d0ad5f647d7d
KrimsonJosh <jumoinzad@gmail.com>
2025-04-14 15:51:52 -0500

    fix: fix to http instead of https so backend server can communicate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
-import './App.css'
-import Dashboard from './pages/Dashboard'
-import React, { useState, useEffect } from "react"
-function App() {
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
+function App() {
+  const isLoggedIn = !!document.cookie.includes('session');
   return (
-    <>
-      <div className = "max-w-xl mx-auto p-4 bg-gray-100 min-h-screen">
-        <h1 className="text-2xl font-bold mb-4">Intern Finance Tracker</h1>
-        <Dashboard />
+    <Router>
+      <div className = "bg-gray-100 min-h-screen">
+        <Routes>
+          {/* Navigate to dashboard if logged in, else enter login page*/}
+          <Route path = "/" element={isLoggedIn ? <Navigate to ="/dashboard"/> : <Login />} />
+          <Route path = "/signup" element = {<Signup />} />
+          <Route path = "/dashboard" element = {isLoggedIn ? <Dashboard /> : <Navigate to = "/" />} /> 
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
